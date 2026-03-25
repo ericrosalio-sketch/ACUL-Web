5a313862650c21a620fdea2d77ca2113ffd5a955
ericrosalio-sketch <eric.rosalio@gmail.com>
2026-03-25 11:01:10 -0700

    index de login-id para el login v2
    no cambia nada, supuestamente. se cambia porque el hash del nombre es distinto por el npm build

---
+++ assets/login-id/index.BIlCIC-8.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{L as p}from"../login/index.CqYEPlCD.js";import"../shared/vendor.DNk1AHy4.js";export{p as default};
+//# sourceMappingURL=index.BIlCIC-8.js.map
