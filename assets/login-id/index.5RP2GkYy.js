fc6177740ce76fc71a3af16d1e21ab2cd4a7048d
ericrosalio-sketch <eric.rosalio@gmail.com>
2026-03-27 10:37:10 -0700

    Ajuste a pantalla Consent v2
    Se intenta evitar que se sobrescriba el color de fondo de Consent. También se cambió el manejo de la función llamada por el botón RECHAZAR para intentar eliminar el error que evita cerrar la sesión, quedando atrapado en la pantalla Consent de una sola cuenta.

---
+++ assets/login-id/index.5RP2GkYy.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{a as m}from"../consent/index.DtDbuTz1.js";import"../shared/vendor.BIKsm7y_.js";export{m as default};
+//# sourceMappingURL=index.5RP2GkYy.js.map
---
+++ assets/login-password/index.DS2ivgdj.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{b as p}from"../consent/index.DtDbuTz1.js";import"../shared/vendor.BIKsm7y_.js";export{p as default};
+//# sourceMappingURL=index.DS2ivgdj.js.map
