5922f067d47c697461f788688d13f447aaf31da7
ericrosalio-sketch <eric.rosalio@gmail.com>
2026-04-07 11:43:16 -0700

    El logo debe poder verse
    Login Id ahora sí debería poder desplegar el logo, antes tenía la posición en none debido a una configuración de Auth0, pero con el Overwrite que usamos ahora, eso es ignorado.

---
+++ assets/login-id/index.8B4eQK2x.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{a as m}from"../consent/index.BSHzlkEY.js";import"../shared/vendor.BIKsm7y_.js";export{m as default};
+//# sourceMappingURL=index.8B4eQK2x.js.map
---
+++ assets/login-password/index.DBDasrqJ.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{b as p}from"../consent/index.BSHzlkEY.js";import"../shared/vendor.BIKsm7y_.js";export{p as default};
+//# sourceMappingURL=index.DBDasrqJ.js.map
