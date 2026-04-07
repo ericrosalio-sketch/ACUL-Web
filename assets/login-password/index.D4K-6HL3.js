83bf877a988aac99ddb51ad1653e914d5af78536
ericrosalio-sketch <eric.rosalio@gmail.com>
2026-04-07 11:15:40 -0700

    Cambio de layout loginId
    La pantalla de Login Id ahora muestra los botones de sociales en la parte inferior. También muestra el logo Noducado en lugar de no tener logo en general.

---
+++ assets/login-password/index.D4K-6HL3.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{b as p}from"../consent/index.DdOojBpy.js";import"../shared/vendor.BIKsm7y_.js";export{p as default};
+//# sourceMappingURL=index.D4K-6HL3.js.map
