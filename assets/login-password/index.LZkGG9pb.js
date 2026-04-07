ac9d985b6b2d73b22f34bd195fbbd15cabede171
ericrosalio-sketch <eric.rosalio@gmail.com>
2026-04-07 11:54:04 -0700

    Logo correcto - La tercera es la vencida
    El logo se ve, pero con esto debería ser el logo correcto. Puede que las dimensiones de la imagen no vayan a ser las correctas pero se sabrá.

---
+++ assets/login-password/index.LZkGG9pb.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{b as p}from"../consent/index.CBRSf7GB.js";import"../shared/vendor.BIKsm7y_.js";export{p as default};
+//# sourceMappingURL=index.LZkGG9pb.js.map
---
+++ assets/login/index.fMe2N_Z1.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.BlyJtN5K.js";import{L as p}from"../consent/index.CBRSf7GB.js";import"../shared/vendor.BIKsm7y_.js";export{p as default};
+//# sourceMappingURL=index.fMe2N_Z1.js.map
