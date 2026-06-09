79c022491268c574ffba4f5127db41d63bccffd5
Eric Rosalio Romero Valdez <eric.rosalio@gmail.com>
2026-06-09 10:26:56 -0700

    modified:   dist/assets/login-id/index.js 	modified:   dist/assets/login-id/index.js.map 	modified:   dist/assets/shared/style.css 	modified:   dist/screens/login-id/login-id/default.json 	modified:   public/screens/login-id/login-id/default.json 	modified:   src/assets/icons/GoogleIcon.tsx 	new file:   src/components/form/ULThemeStaticLabelField.tsx 	modified:   src/components/form/index.tsx 	modified:   src/screens/signup-id/components/AlternativeLogins.tsx 	modified:   src/screens/signup-id/components/SignupIdForm.tsx 	modified:   src/utils/helpers/iconUtils.tsx

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,2 +1,3 @@
 export { ULThemeFloatingLabelField } from "./ULThemeFloatingLabelField";
 export { ULThemeFormMessage } from "./ULThemeFormMessage";
+export { ULThemeStaticLabelField } from "./ULThemeStaticLabelField";
