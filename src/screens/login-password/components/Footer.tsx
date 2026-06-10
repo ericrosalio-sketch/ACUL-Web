cb661636ca017b41105f2386e55e0465e356fc07
Fausto Garcia <fausto.garcia@coppel.com>
2026-06-10 10:33:59 -0700

    login-password

---
+++ src/screens/login-password/components/Footer.tsx
@@ -0,0 +1,25 @@
+import ULThemeLink from "@/components/ULThemeLink";
+
+import { useLoginPasswordManager } from "../hooks/useLoginPasswordManager";
+import { AlignCenter } from "lucide-react";
+
+function Footer() {
+  const { signupLink, texts, locales } = useLoginPasswordManager();
+
+  if (!signupLink) {
+    return null;
+  }
+
+  return (
+    <div className="mt-4 text-center">
+      <span className="pr-1 text-body-text text-(length:--ul-theme-font-body-text-size) font-body">
+        {texts?.signupActionText || locales?.footer?.signupActionText}
+      </span>
+      <ULThemeLink href={signupLink}>
+        {texts?.signupActionLinkText || locales?.footer?.signupActionLinkText}
+      </ULThemeLink>
+    </div>
+  );
+}
+
+export default Footer;
