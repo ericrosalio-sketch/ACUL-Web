4a8def2594c2c0d53d6695ef49252da9adbc416c
Eric Romero <eric.romero@coppel.com>
2026-06-20 14:42:59 -0700

    Deploy dist to gh-pages

---
+++ assets/signup-id/index.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.js";import"../login-id/index.js";import"../shared/vendor.js";
+//# sourceMappingURL=index.js.map
--- dist/assets/login-password/index.js
+++
@@ -1,2 +0,0 @@
-import"../shared/react-vendor.js";import{L as p}from"../login-id/index.js";import"../shared/vendor.js";export{p as default};
-//# sourceMappingURL=index.js.map
